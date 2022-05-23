ea9880597813e3804d8bb3fb2921932e4b28d958
HMojadidi <95712234+HMojadidi@users.noreply.github.com>
2022-05-23 15:08:27 -0400

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,18 +32,19 @@ L.geoJson(sanFranAirport, {
     pointToLayer: function(feature, latlng) {
       console.log(feature);
       return L.marker(latlng)
-      .bindPopup("<h2>" + feature.properties.city + "</h2>");
+      .bindPopup(`<h2>${'Airport Code: '+feature.properties.faa}</h2><hr><h2>${'Airport Name: '+feature.properties.name}</h2><h2>`);
     }
 
   }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
-let graymap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  const graymap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
     maxZoom: 18,
-    id: 'mapbox.streets',
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
     accessToken: API_KEY
-});
-
+  }).addTo(map);
+  
 // Then we add our 'graymap' tile layer to the map.
 graymap.addTo(map);
